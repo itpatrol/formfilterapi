89dba41f2abb3e51557f71c3ee81c246398e61f0
Sergey Pavlenko <sergeypavlenko@1343804.no-reply.drupal.org>
2015-04-03 18:58:52 +0300

    Fixed #13: New hook_formfilterapi_alter(). Alter $filters array.

--- formfilterapi.api.php
+++ formfilterapi.api.php
@@ -11,28 +11,53 @@
  * Via this hook possible to change variables before processing their module.
  * And add your form elements, for filtering.
  *
- * @param string $op
+ * @param string $operation
  *   Name of the operation is executed.
- *
  * @param string $session_name
- *   The session name form.
+ *   The unique session name.
  *
- * @param array $sessvals
- *   Field before filtration.
+ * @return array
+ *   Form with filter elements.
  */
-function hook_formfilterapi($op, $session_name, &$sessvals = array()) {
-  $example_session_name = 'test';
-
-  switch ($op) {
+function hook_formfilterapi($operation, $session_name) {
+  switch ($operation) {
     case 'filters':
-      if ($session_name == $example_session_name) {
+      if ($session_name == 'test') {
         $form['title'] = array(
           '#type' => 'textfield',
           '#title' => t('Title'),
-          '#where' => array('like' => "n.title"),
+          '#where' => array('like' => 'n.title'),
         );
 
         return $form;
+      }
+      break;
+  }
+}
+
+/**
+ * Alter filters for hook_formfilterapi().
+ *
+ * @param array $filters
+ *  Filters from hook_formfilterapi().
+ * @param string $session_name
+ *   The unique session name.
+ * @param string $operation
+ *   Name of the operation is executed.
+ */
+function hook_formfilterapi_alter(&$filters, $session_name, $operation) {
+  switch ($operation) {
+    case 'filters':
+      if ($session_name == 'test') {
+        $filters['sticky'] = array(
+          '#type' => 'select',
+          '#title' => t('Sticky at top of lists'),
+          '#options' => array(
+            '1' => t('Yes'),
+            '0' => t('No'),
+          ),
+          '#where' => 'n.sticky',
+        );
       }
       break;
   }
